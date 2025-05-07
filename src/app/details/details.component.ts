9a49049a04ec1ac29f742a06cbd3850a721036da
delafuentelupiita <delafuentelupiita@gmail.com>
2025-05-07 11:34:21 -0600

    Se agrega formulario para solicitar vivienda.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   standalone: true, 
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <!-- <p>
       details works! {{ housingLocation?.id }}
     </p> -->
     <article>
-      <img class="listing-photo" [src]="housingLocation?.photo">
+      
       <section class="listing-description">
+      <img class="listing-photo" [src]="housingLocation?.photo">
         <h2 class="listing-heading">{{housingLocation?.name}}</h2>
         <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}
       </section>
@@ -28,7 +31,19 @@ import { HousingLocation } from '../housing-location';
       </section>
       <section class="listing-apply">
         <h2 class="section-heading">Apply now to live here</h2>
-        <button class="primary" type="button">Apply now</button>
+        <!-- <button class="primary" type="button">Apply now</button> -->
+         <form [formGroup]="applyForm" (submit)="submitApplication()">
+            <label for="first-name">First Name</label>
+            <input id="first name" type="text" formControlName="firstName">
+
+            <label for="last-name">Last Name</label>
+            <input id="last-name" type="text" formControlName="lastName">
+
+            <label for="email">Email</label>
+            <input id="email" type="email" formControlName="email">
+
+            <button type="submit" class="primary">Apply now</button>
+         </form>
       </section>
   `,
   styleUrls: ['./details.component.css']
@@ -38,10 +53,23 @@ export class DetailsComponent {
   // housingLocationId = 0;
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup ({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  })
 
   constructor(){
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication(){
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
   
